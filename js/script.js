5e56345ad66cba6c52a99c3d5c139636d066d415
Weronika Makuch <weronika.b.makuch@gmail.com>
2023-04-05 00:25:28 +0200

    playerMoves

--- js/script.js
+++ js/script.js
@@ -5,9 +5,21 @@ if (randomNumber == '1') {
   computerMove = 'kamień';
 } else if (randomNumber == '2') {
   computerMove = 'papier';
-} else if (randomNumber == '3') {
+} else (randomNumber == '3') {
   computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
-}
+} 
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
